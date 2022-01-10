285d90aea4f6a5bf0f21ad56318d7fcf2375c530
Gaurav <gaurav.s@exzeo.com>
2022-01-10 16:44:14 +0530

    Initial Commit

---
+++ Ransom/RansomApp.swift
@@ -0,0 +1,20 @@
+//
+//  RansomApp.swift
+//  Ransom
+//
+//  Created by Gaurav Singh on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RansomApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
