326b05f6080b3a869f0e4903632434f88fb172fd
Gaurav <gaurav.s@exzeo.com>
2022-01-10 17:09:14 +0530

    Home Screen Added

---
+++ Ransom/HomeScreen.swift
@@ -0,0 +1,22 @@
+//
+//  HomeScreen.swift
+//  Ransom
+//
+//  Created by Gaurav Singh on 10/01/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text("Hello SwiftUI")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
